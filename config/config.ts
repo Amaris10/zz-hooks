85c925033c817db275590a8cab315c706b3a0d45
“amaris10” <amaris10@126.com>
2023-12-28 20:44:14 +0800

    feat: pages

---
+++ config/config.ts
@@ -0,0 +1,62 @@
+export default {
+  base: '/zz-hooks',
+  theme: {
+    // 修改 dumi 默认主题的主色，更多变量详见：https://github.com/umijs/dumi/blob/1.x/packages/theme-default/src/style/variables.less
+    '@c-primary': '#EDA667',
+  },
+  exportStatic: {},
+  nodeModulesTransform: {
+    type: 'none',
+    exclude: [],
+  },
+  publicPath: '/zz-hooks/',
+  history: { type: 'hash' },
+  extraBabelPlugins: [
+    [
+      'babel-plugin-import',
+      {
+        libraryName: '@alifd/next',
+        style: false,
+      },
+      'fusion',
+    ],
+  ],
+  mode: 'site',
+  title: 'zz-hooks',
+  favicon: '/zizi.svg',
+  logo: '/zizi.svg',
+  dynamicImport: {},
+  manifest: {},
+  hash: true,
+  alias: {
+    //encodeHooks: process.cwd() + '/packages/hooks/src/index.ts',
+  },
+  resolve: {
+    includes: ['docs', 'packages/hooks/src'],
+  },
+  links: [
+    {
+      rel: 'stylesheet',
+      href: 'https://unpkg.com/@alifd/theme-design-pro@0.6.2/dist/next-noreset.min.css',
+    },
+    { rel: 'stylesheet', href: '/style.css' },
+  ],
+  navs: [
+    { title: '指南', path: '/guide' },
+    { title: 'Hooks', path: '/hooks' },
+  ],
+  menus: {
+    '/': [
+      {
+        title: '首页',
+        path: 'index',
+      },
+    ],
+    '/guide': [
+      {
+        title: '介绍',
+        path: '/guide',
+      },
+    ],
+  },
+};
